77ca3b87d642fcb5708ab19a38cb80ec0b7a80b8
Empedev <maciej@cleancommit.io>
2021-11-22 22:46:11 +0100

    deplay fix #1

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-// app.use(express.static(path.resolve(__dirname, './client/dist')))
+app.use(express.static(path.resolve(__dirname, './client/dist')))
 // app.get('*', function (request, response) {
 //   response.sendFile(path.resolve(__dirname, './client/dist', 'index.html'))
 // })
